ed6189779d8ae71a11c3fd2ea2e79d68cff3aa26
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-31 14:12:14 +0900

    チャットルーム名を表示

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_134950) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
